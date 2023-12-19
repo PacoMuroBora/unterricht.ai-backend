7898135d753f1792ddb7538c7f76d13d8ad2c231
Paco Muro <paco@studioborabora.com>
2023-12-19 23:07:17 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+
+const __dirname = path.resolve();
+const port = 3002;
+
+/// express setup
+const app = express();
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+/// serve client
+app.use(express.static(path.join(__dirname, 'public')));
+
+/// routing
+app.get('/api/data', (req, res) => {
+  // Handle API requests here
+  res.json({ message: 'Hello from Express!' });
+});
+
+// app.use("/api/ai", apiGame);
+
+// const httpServer = http.createServer(app);
+// httpServer.listen(3000);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
