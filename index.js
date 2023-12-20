a66740dd2efe16817d59cbeaea4f4a8575b711ee
Paco Muro <paco@studioborabora.com>
2023-12-20 10:36:10 +0100

    tests section

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
-// import bodyParser from 'body-parser';
+import bodyParser from 'body-parser';
 import express from 'express';
-// import cors from 'cors';
+import cors from 'cors';
 import path from 'path';
 import filesService, { uploadPDF } from './api/files-service.js';
 import aiService, { getStandaloneQuestion } from './api/ai-service.js';
@@ -10,12 +10,12 @@ const __dirname = path.resolve();
 const port = 3002;
 /// express setup
 const app = express();
-// app.use(cors());
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: true,
-//   })
-// );
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 /// serve client
 app.use(express.static(path.join(__dirname, 'public')));
@@ -32,7 +32,10 @@ app.use('/api/ai', aiService);
 // const httpServer = http.createServer(app);
 // httpServer.listen(3000);
 
+/// tests
+
 // uploadPDF('./testData/Pedagogy.pdf');
+
 // getStandaloneQuestion(
 //   `I am not sure but if I could I would really like to travel to a warm country with beaches.
 //   I don't even know how I could afford it but I wonder which place that would be!`
