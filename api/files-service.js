2fc44e906f28375ac3a2f0bd7e72d605e94bc506
Paco Muro <paco@studioborabora.com>
2024-01-27 19:24:50 +0100

    file upload

--- api/files-service.js
+++ api/files-service.js
@@ -1,22 +1,29 @@
 import express from 'express';
 import multer from 'multer';
 import fs from 'fs';
+import { supabaseClient } from '../utils/supabase.js';
 
 const upload = multer({ dest: 'uploads/' }); // Set the destination folder for uploaded files
 const router = express.Router();
 
+const testUserId = '2e4ace06-270b-429b-9726-f937261ed293';
+
 router.post('/upload', upload.single('file'), async (req, res) => {
+  let _path;
   try {
     if (!req.file) {
       return res.status(400).json({ error: 'No file uploaded' });
     }
 
     if (!req.user) {
-      req.user = { id: '000' };
+      req.user = { id: testUserId };
     }
 
     // Access the uploaded file details
     const { originalname, mimetype, size, path, buffer } = req.file;
+    _path = path;
+    const fileExtension = originalname.split('.').pop();
+    const { filename } = req.body;
 
     // Check file type
     if (!['text/plain', 'application/pdf'].includes(mimetype)) {
@@ -25,24 +32,56 @@ router.post('/upload', upload.single('file'), async (req, res) => {
       return res.status(400).json({ error: 'Invalid file type' });
     }
 
-    console.log('File Details:', { originalname, mimetype, size, path });
+    // console.log('File Details:', { originalname, mimetype, size, path });
 
-    // Upload file to Supabase Storage
-    const { data: fileData, error: fileError } = await supabase.storage
+    // TODO same file name handling
+    // Check if a file with the same name exists
+    // const { data: existingFiles, error: existingFilesError } =
+    //   await supabaseClient.storage
+    //     .from('context_uploads')
+    //     .list(`user_${req.user.id}/${originalname}`);
+
+    // console.log(existingFiles);
+
+    // if (existingFilesError) {
+    //   console.log('filename did not exist');
+    //   // throw existingFilesError;
+    // }
+
+    // let finalFileName = originalname;
+
+    // // If a file with the same name exists, add a suffix "(1)"
+    // if (existingFiles && existingFiles.length > 0) {
+    //   const suffix = 1;
+    //   const fileNameWithoutExtension = originalname.replace(/\.[^/.]+$/, ''); // Remove file extension
+    //   finalFileName = `${fileNameWithoutExtension}_${suffix}.${fileExtension}`;
+    // }
+
+    // Upload file to Supabase Storage fileData: {path, id, fullPath}
+    const { data: fileData, error: fileError } = await supabaseClient.storage
       .from('context_uploads')
-      .upload(`user_${req.user.id}/${originalname}`, buffer);
+      .upload(`user_${req.user.id}/${filename}.${fileExtension}`, buffer);
 
     if (fileError) {
       throw fileError;
     }
 
     // Update the 'uploaded_docs' array in the 'profiles' table
-    const { data: profileData, error: profileError } = await supabase
+    const { data: profileData, error: profileError } = await supabaseClient
       .from('profiles')
-      .update({
-        uploaded_docs: supabase.sql`array_append(uploaded_docs, ${fileData[0].id})`,
-      })
-      .eq('user_id', req.user.id);
+      .upsert(
+        [
+          {
+            id: req.user.id,
+            uploaded_docs: [fileData.fullPath],
+          },
+        ],
+        { onConflict: ['id'], merge: ['uploaded_docs'] }
+      );
+
+    if (profileError) {
+      throw profileError;
+    }
 
     // Remove the uploaded file
     fs.unlinkSync(path);
@@ -54,6 +93,8 @@ router.post('/upload', upload.single('file'), async (req, res) => {
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'Internal server error' });
+
+    fs.unlinkSync(_path);
   }
 });
 
