08d823e889f09f7a5a0d20f8821cf8d1e879d1d8
Civan Erbay <info@civan-erbay.de>
2024-01-28 15:12:58 +0100

    Fix body req and add logs

--- api/ai-service.js
+++ api/ai-service.js
@@ -1,21 +1,25 @@
-import express from 'express';
-import axios from 'axios';
+import express from 'express'
+import axios from 'axios'
 
-const router = express.Router();
+const router = express.Router()
 
 router.get('/', async (req, res) => {
-  res.json({ msg: 'sdfysf' });
-});
+  res.json({ msg: 'sdfysf' })
+})
 
 router.post('/prompt', async (req, res) => {
-  const { prompt } = req.body;
+  const prompt = req.body
+
+  console.log({ prompt })
 
   const response = await axios.post(
     'https://unterricht-ai-backend-wfw8.onrender.com/prompt',
     { query: prompt }
-  );
+  )
 
-  res.json({ response: response.data });
-});
+  console.log({ response })
 
-export default router;
+  res.json({ response: response.data })
+})
+
+export default router
