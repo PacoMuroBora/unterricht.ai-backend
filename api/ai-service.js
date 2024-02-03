94bc1240ebb26cb82618d0cdbcaeb70c048aa488
Paco Muro <paco@studioborabora.com>
2024-02-03 11:07:07 +0100

    fix ai service url

--- api/ai-service.js
+++ api/ai-service.js
@@ -1,34 +1,31 @@
-import express from 'express'
-import axios from 'axios'
+import express from 'express';
+import axios from 'axios';
 
-const router = express.Router()
+const router = express.Router();
 
-//const aiBackendUrl = 'http://unterricht-ai-service:10000';
-const aiBackendUrl =
-  'https://unterricht-ai-backend-wfw8.onrender.com/prompt'
+const aiBackendUrl = 'http://unterricht-ai-service:10000';
+// const aiBackendUrl =
+//   'https://unterricht-ai-backend-wfw8.onrender.com/prompt'
 
 router.get('/ping', async (req, res) => {
   try {
-    const response = await axios.get(`${aiBackendUrl}`)
-    res.json({ response: response.data })
+    const response = await axios.get(`${aiBackendUrl}`);
+    res.json({ response: response.data });
   } catch (error) {
-    console.error('Error:', error)
-    res.status(500).json({ error: 'Internal server error' })
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal server error' });
   }
-})
+});
 
 router.post('/prompt', async (req, res) => {
   try {
-    const response = await axios.post(
-      `${aiBackendUrl}/prompt`,
-      req.body
-    )
-    console.log(response)
-    res.json({ response: response.data })
+    const response = await axios.post(`${aiBackendUrl}/prompt`, req.body);
+    console.log(response);
+    res.json({ response: response.data });
   } catch (error) {
-    console.error('Error:', error)
-    res.status(500).json({ error: 'Internal server error' })
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal server error' });
   }
-})
+});
 
-export default router
+export default router;
